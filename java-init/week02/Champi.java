846bc3c646512bacf1bc2bf1be1dfbf5c74cb929
Martin Courtois <martin.courtois@protonmail.com>
2021-07-01 22:28:15 +0200

    [ADD] week02

--- java-init/week02/Champi.java
+++ java-init/week02/Champi.java
@@ -1,17 +1,57 @@
-System.out.println("Pensez a un champignon : amanite tue mouches, pied bleu, girolle,");
-System.out.println("cèpe de Bordeaux, coprin chevelu ou agaric jaunissant.");
+import java.util.Scanner;
 
-System.out.print("Est-ce que votre champignon a des lamelles (true : oui, false : non) ? ");
-System.out.print("Est-ce que votre champignon a un anneau (true : oui, false : non) ? ");
-System.out.print("Est-ce que votre champignon a un chapeau convexe (true : oui, false : non) ? ");
-System.out.print("Est-ce que votre champignon vit en forêt (true : oui, false : non) ? ");
+public class Champi {
+    static final String CHAMP_1 = "l'agaric jaunissant.";
+    static final String CHAMP_2 = "l'amanite tue-mouches.";
+    static final String CHAMP_3 = "le cèpe de Bordeaux.";
+    static final String CHAMP_4 = "le coprin chevelu.";
+    static final String CHAMP_5 = "la girolle.";
+    static final String CHAMP_6 = "le pied bleu.";
+    static final String QUESTION_1 = "Est-ce que votre champignon a des lamelles (true : oui, false : non) ? ";
+    static final String QUESTION_2 = "Est-ce que votre champignon vit en forêt (true : oui, false : non) ? ";
+    static final String QUESTION_3 = "Est-ce que votre champignon a un chapeau convexe (true : oui, false : non) ? ";
+    static final String QUESTION_4 = "Est-ce que votre champignon a un anneau (true : oui, false : non) ? ";
 
-System.out.print("==> Le champignon auquel vous pensez est ");
+    public static void main(String[] args) {
+        System.out.println("Pensez a un champignon : amanite tue mouches, pied bleu, girolle,");
+        System.out.println("cèpe de Bordeaux, coprin chevelu ou agaric jaunissant.");
 
-System.out.print("l'agaric jaunissant");
-System.out.print("l'amanite tue-mouches");
-System.out.print("le cèpe de Bordeaux");
-System.out.print("le coprin chevelu");
-System.out.print("la girolle");
-System.out.print("le pied bleu");
+        String champignon;
+        boolean q = false;
+        Scanner clavier = new Scanner(System.in);
 
+        System.out.print(QUESTION_3);
+        q = clavier.nextBoolean();
+
+        if (q) {
+            System.out.print(QUESTION_2);
+            q = clavier.nextBoolean();
+            if (q) {
+                System.out.print(QUESTION_4);
+                q = clavier.nextBoolean();
+                if (q) {
+                    champignon = CHAMP_2;
+                } else {
+                    champignon = CHAMP_6;
+                }
+            } else {
+                champignon = CHAMP_1;
+            }
+        } else {
+            System.out.print(QUESTION_4);
+            q = clavier.nextBoolean();
+            if (q) {
+                champignon = CHAMP_4;
+            } else {
+                System.out.print(QUESTION_1);
+                q = clavier.nextBoolean();
+                if (q) {
+                    champignon = CHAMP_5;
+                } else {
+                    champignon = CHAMP_3;
+                }
+            }
+        }
+        System.out.print("==> Le champignon auquel vous pensez est " + champignon);
+    }
+}
--- java-init/week02/Velo.java
+++ java-init/week02/Velo.java
@@ -12,14 +12,42 @@ public class Velo {
         /*******************************************
          * Completez le programme a partir d'ici.
          *******************************************/
+        int tarifA0 = 0;
+        int tarifA1 = 7;
+        int tarifB0 = 7;
+        int tarifB1 = 17;
+        int tarifC0 = 17;
+        int tarifC1 = 24;
 
-        System.out.println("Les heures doivent être comprises entre 0 et 24 !");
-        System.out.println("Bizarre, vous n'avez pas loué votre vélo bien longtemps !");
-        System.out.println("Bizarre, le début de la location est après la fin ...");
-        System.out.println("Vous avez loué votre vélo pendant");
-        System.out.print("Le montant total à payer est de ");
-        System.out.println(" franc(s).");
 
+        if (!(debut >=0 && debut <= 24) || !(fin >=0 && fin <= 24)){
+            System.out.println("Les heures doivent être comprises entre 0 et 24 !");
+        } else if (fin == debut) {
+            System.out.println("Bizarre, vous n'avez pas loué votre vélo bien longtemps !");
+        } else if (fin < debut) {
+            System.out.println("Bizarre, le début de la location est après la fin ...");
+        } else {
+            int timeA = Math.max(0, Math.min(tarifA1, fin) - Math.max(tarifA0, debut));
+            int timeB = Math.max(0, Math.min(tarifB1, fin) - Math.max(tarifB0, debut));
+            int timeC = Math.max(0, Math.min(tarifC1, fin) - Math.max(tarifC0, debut));
+            int total = 0;
+
+            System.out.println("Vous avez loué votre vélo pendant");
+            if (timeA + timeC > 0) {
+                total += timeA + timeC * 1;
+                System.out.print(timeA + timeC);
+                System.out.print(" heure(s) au tarif horaire de " + 1.0);
+                System.out.println(" franc(s)");
+            }
+            if (timeB > 0) {
+                total += timeB * 2;
+                System.out.print(timeB);
+                System.out.print(" heure(s) au tarif horaire de " + 2.0);
+                System.out.println(" franc(s)");
+            }
+            System.out.print("Le montant total à payer est de " + (double)total);
+            System.out.println(" franc(s).");
+        }
         /*******************************************
          * Ne rien modifier apres cette ligne.
          *******************************************/
